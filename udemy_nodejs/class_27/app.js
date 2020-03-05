e0d33378201572b299a79fd57de519fb8fbee3d7
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-05 19:04:11 -0300

    Working with require function from node.js, understanding how parameters exports and module.exports works; Understanding how mutate objects

---
+++ udemy_nodejs/class_27/app.js
@@ -0,0 +1,5 @@
+var greet = require("./greet");
+
+// mutating objects: changing objects, for example, when you add a method or a property to an object
+var greet2 = require("./greet2");
+greet2.greet();
