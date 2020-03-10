c621035bef4d105390e9da85685958246b2fc993
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-10 19:50:27 -0300

    removing directories

--- udemy_nodejs/aula_18/app.js
+++
@@ -1,9 +0,0 @@
-// para incluir módulos
-// require("./greet.js");
-
-// usando o module.exports para expor uma funcao e capturando ela
-var greet = require("./greet");
-greet();
-
-
-
--- udemy_nodejs/aula_18/greet.js
+++
@@ -1,8 +0,0 @@
-// meu primeiro módulo
-
-var greet = function(){
-    console.log("Hello, Im greet.js, and I was required. (:"); 
-}
-
-// escolhendo o que será exposto do módulo para uso externo
-module.exports = greet;
