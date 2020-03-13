9d9e419f630f3cb71551ca736f302859acb527d5
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-13 12:20:03 -0300

    Including parameters to the listeners and chain of objects

--- udemy_nodejs/section_5/class_36/app.js
+++ udemy_nodejs/section_5/class_36/app.js
@@ -11,14 +11,17 @@ function Greetr() {
 // All Greetr objects will get access to EventEmitter methods and properties
 util.inherits(Greetr, EventEmitter);
 
-Greetr.prototype.greet = function() {
-    console.log(this.greeting);
-    this.emit('greet');// comming from EventEmitter
+Greetr.prototype.greet = function(data) {
+    console.log(this.greeting + ' ' + data);
+    // here we can include the list of parameters to send to whoelse is listening to this function
+    this.emit('greet', data);// comming from EventEmitter
+
 }
 
 var greetr1 = new Greetr();
-greetr1.on('greet', function() {
+// Prepare the listener to expect the parameter
+greetr1.on('greet', function(data) {
     console.log('Someone greeted!');
 });
 
-greetr1.greet();
+greetr1.greet('Tony');
