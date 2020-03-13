9e9077ff2dcc2da7dbfecdc72436c022888a68dd
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-13 13:16:44 -0300

    Working with classes in JS

--- udemy_nodejs/section_5/class_40/app.js
+++ udemy_nodejs/section_5/class_40/app.js
@@ -2,13 +2,22 @@
 // new way to build objects, only in syntax
 // so its called Syntatic Sugar - a feature that changes how you type, but doesnt change the behaviour
 
-function Person(firstname, lastname) {
-    this.firstname = firstname;
-    this.lastname = lastname;
-}
+// Changing to use new ES6 classes
+// helps u not making silly mistakes, so it helps compiling with the best way
+'use sctrict';
 
-Person.prototype.greet = function(){
-    console.log(`Hello ${ this.firstname} ${this.lastname}`);
+// from ES6, its a cleaner way to build the code and setting properties and methods
+// NOTE: in JS classes is just syntax sugar
+// this also can confuse about prototype chain, that isnt as in other languages, where classes are more complexx
+class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    greet() {
+        console.log(`Hello ${ this.firstname} ${this.lastname}`);
+    }
 }
 
 var john = new Person('John','Doe');
