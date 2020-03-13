3299fef463cef7b8ef8c64f7c1e30141a251ac3f
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-12 21:05:09 -0300

    Changing the use of a pattern to deal better with strings

--- udemy_nodejs/section_5/class_34/app.js
+++ udemy_nodejs/section_5/class_34/app.js
@@ -1,12 +1,17 @@
 var Emitter = require('events');
+var eventConfig = require('./config').events;
 
 var emtr = new Emitter();
 
-emtr.on('greet', function(){
+// Magic Strings: a string that has some special meaning in our code
+// So it can causes a bug
+
+// One way to deal with this with a pattern (similar to ENUM):
+emtr.on(eventConfig.GREET, function(){
     console.log("Somewhere, someone said Hello. ");
 });
 
-emtr.on('greet', function(){
+emtr.on(eventConfig.GREET, function(){
     console.log("A greeting occurred!");
 });
 
