2a3cb264c25b06b0ef5ac6e4120f2498430e5183
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-13 17:29:44 -0300

    class 50 - working with assynchronous code, callbacks and reading files

---
+++ udemy_nodejs/section_6/class_50/app.js
@@ -0,0 +1,23 @@
+// node module to work with filesystem
+var fs = require('fs');
+
+// method to obtain the text from file
+// the __dirname gives the path where this app is running
+// the readFileSync is a synchronous approach to wait till the buffer is filled, before the engine run the rest of code
+var greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
+console.log(greet);
+
+// If we want to work with file assynchronously, we pass a callback function
+var greet2 = fs.readFile(
+    __dirname + '/greet.txt', 
+    'utf8', // when we set the encoding, it automatically converts the response to String
+     function(err, data){
+         if (err) {
+             console.log(`An error occured: ${error}`);
+         }
+         console.log(`Data retrieved: ${data}`);
+        console.log(data);
+    }
+);
+
+console.log('Done!');
