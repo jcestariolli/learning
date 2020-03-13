fccc34fcdfe0f01bf95a8e7f298d74cdb462a693
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-13 17:31:17 -0300

    class 50 - refactor for file name

--- udemy_nodejs/section_6/class_50/app.js
+++ udemy_nodejs/section_6/class_50/app.js
@@ -1,15 +1,17 @@
 // node module to work with filesystem
 var fs = require('fs');
 
+var greetFile = '/greet.txt';
+
 // method to obtain the text from file
 // the __dirname gives the path where this app is running
 // the readFileSync is a synchronous approach to wait till the buffer is filled, before the engine run the rest of code
-var greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
+var greet = fs.readFileSync(__dirname + greetFile, 'utf8');
 console.log(greet);
 
 // If we want to work with file assynchronously, we pass a callback function
 var greet2 = fs.readFile(
-    __dirname + '/greet.txt', 
+    __dirname + greetFile, 
     'utf8', // when we set the encoding, it automatically converts the response to String
      function(err, data){
          if (err) {
