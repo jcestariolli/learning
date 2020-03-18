d8f7de7880d40f065c8db3c0cde99e1a32310a78
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-18 18:11:20 -0300

    class 75 - using and creating middleware

--- udemy_nodejs/section_9/class_75/jeka/app.js
+++ udemy_nodejs/section_9/class_75/jeka/app.js
@@ -3,12 +3,22 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+// to work with requested files automatically, we can use express like above
+// the express.static() is a middleware to work with the files for us
+// that means that whenever i call some url like /assets/file_name.extension, express will look for it inside the current dir / public / file_name.extension
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.use('/', function (req, res, next) {
+	console.log('Request Url:' + req.url);
+	next();
+});
+
 app.get('/', function(req, res) {
-	res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
+	res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello world!</h1></body></html>');
 });
 
 app.get('/person/:id', function(req, res) {
-	res.send('<html><head></head><body><h1>Person: ' + req.param.id + '</h1></body></html>');
+	res.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
 });
 
 app.get('/api', function(req, res) {
