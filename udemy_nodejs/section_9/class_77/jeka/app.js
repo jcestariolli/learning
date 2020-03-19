f8f02637796d0cd843399435ffb1415c93782d07
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-19 18:39:56 -0300

    Class 77 - configuring body-parser to deal with POST methods and attach the variables on the request

--- udemy_nodejs/section_9/class_77/jeka/app.js
+++ udemy_nodejs/section_9/class_77/jeka/app.js
@@ -1,12 +1,18 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 
+
+var app = express();
 var port = process.env.PORT || 3000;
+// copied from body-parser page 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 
 app.use('/assets', express.static(__dirname + '/public'));
-
 app.set('view engine', 'ejs');
 
+
+// Routing
 app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
 	next();
@@ -21,8 +27,20 @@ app.get('/person/:id', function(req, res) {
 	res.render('person', { ID: req.params.id , QueryString: req.query.qrstr});
 });
 
+// POST for the same route
+// urlencodedParser will be the middleware to be called before the code gets here
+app.post('/person', urlencodedParser, function(req, res) {
+	res.send('Thank you person!');
+	// body parser automatically attachs the parameters inside the req object and attach the properties that came on the post method
+	console.log(req.body.firstname);
+	console.log(req.body.lastname);
+});
+
+
 app.get('/api', function(req, res) {
 	res.json({ firstname: 'John', lastname: 'Doe' });
 });
 
+
+// Application start
 app.listen(port);
