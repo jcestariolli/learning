26a545e1aa387416289c0d0ee5576ac9fcce7a7b
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-19 20:03:58 -0300

    Class 79 - Structuring my app to use modules and organize the app

--- udemy_nodejs/section_9/class_79/jeka/app.js
+++ udemy_nodejs/section_9/class_79/jeka/app.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var apiController = require('./controllers/apiController');
+var htmlController = require('./controllers/htmlController');
+
+
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -16,31 +20,6 @@ app.use('/', function (req, res, next) {
 	next();
 });
 
-app.get('/', function(req, res) {
-	res.render('index');
-});
-
-app.get('/person/:id', function(req, res) {
-	res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
-});
-
-app.post('/person', urlencodedParser, function(req, res) {
-	res.send('Thank you!');
-	console.log(req.body.firstname);
-	console.log(req.body.lastname);
-});
-
-app.get('/api/person/:id', function(req, res) {
-	// get that data from database
-	res.json({ firstname: 'John', lastname: 'Doe' });
-});
-
-app.post('/api/person', jsonParser, function(req, res) {
-	// save to the database
-});
-
-app.delete('/api/person/:id', function(req, res) {
-	// delete from the database
-});
-
+htmlController(app);
+apiController(app);
 app.listen(port);
