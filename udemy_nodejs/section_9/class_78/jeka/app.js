27644b885b661466f2d06c6e7feacdcd53037321
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-19 18:51:07 -0300

    Class 78 - Updating files

--- udemy_nodejs/section_9/class_78/jeka/app.js
+++ udemy_nodejs/section_9/class_78/jeka/app.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+
+
 var app = express();
-
 var port = process.env.PORT || 3000;
-
+// copied from body-parser page 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var jsonParser = bodyParser.json();
 
 app.use('/assets', express.static(__dirname + '/public'));
-
 app.set('view engine', 'ejs');
 
+
+// Routing
 app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
 	next();
@@ -20,12 +22,16 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+// express provides an engine to process query string
 app.get('/person/:id', function(req, res) {
-	res.render('person', { ID: req.params.id, Qstr: req.query.qstr });
+	res.render('person', { ID: req.params.id , QueryString: req.query.qrstr});
 });
 
+// POST for the same route
+// urlencodedParser will be the middleware to be called before the code gets here
 app.post('/person', urlencodedParser, function(req, res) {
-	res.send('Thank you!');
+	res.send('Thank you person!');
+	// body parser automatically attachs the parameters inside the req object and attach the properties that came on the post method
 	console.log(req.body.firstname);
 	console.log(req.body.lastname);
 });
@@ -36,8 +42,11 @@ app.post('/personjson', jsonParser, function(req, res) {
 	console.log(req.body.lastname);
 });
 
+
 app.get('/api', function(req, res) {
 	res.json({ firstname: 'John', lastname: 'Doe' });
 });
 
+
+// Application start
 app.listen(port);
