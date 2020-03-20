79f32fd65e48ccf205fbe3be0b7be29e3a8f198a
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-19 23:16:11 -0300

    class 83 - using mongoose

--- udemy_nodejs/section_10/class_83/jeka/app.js
+++ udemy_nodejs/section_10/class_83/jeka/app.js
@@ -1,6 +1,52 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb+srv://test_jeka:<password_goes_here>@jekalearning-9ahk8.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Constructs the Schema
+var Schema = mongoose.Schema;
+
+// Models the schema
+var personSchema = new Schema({
+	firstname: String,
+	lastname: String,
+	address: String
+});
+
+// Creates the schema inside the db
+var Person = mongoose.model('Person', personSchema);
+
+// CReates new Instances of this document (for mongo) and object (for js)
+var john = Person({
+  firstname: 'John',
+  lastname: 'Doe',
+  address: '555 Main St.'
+});
+
+// save the user
+john.save(function(err) {
+  if (err) throw err;
+
+  console.log('person saved!');
+});
+
+var jane = Person({
+  firstname: 'Jane',
+  lastname: 'Doe',
+  address: '555 Main St.'
+});
+
+// save the user
+jane.save(function(err) {
+  if (err) throw err;
+
+  console.log('person saved!');
+});
+
+
+
+// OLD CODE
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
 
@@ -14,7 +60,7 @@ app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
 	next();
 });
-
+//ACorMaisQuente1 test_jeka
 htmlController(app);
 
 apiController(app);
