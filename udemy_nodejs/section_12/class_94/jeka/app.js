9e6cefb19422a280b4bdbc6d831aab2802eaaee2
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-20 16:13:02 -0300

    Class 94 - including files for the exercise

---
+++ udemy_nodejs/section_12/class_94/jeka/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var config = require('./config'); // Our config
+
+// Creates the app
+var app = express();
+
+// Configures the port
+var port = process.env.port || 3000;
+
+// Define the dir public as the path assets to static access them
+app.use('/assets', express.static(__dirname + '/public'));
+
+// Define the view engine 
+app.set('view engine', 'ejs')
+
+// Configuring the DB
+mongoose.connect(config.getDbConnectionString());
+
+
+
+// Listen to that port
+app.listen(port);
+
