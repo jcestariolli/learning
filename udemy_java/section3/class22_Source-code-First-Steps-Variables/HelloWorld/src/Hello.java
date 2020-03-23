6f705021d58850b91bf5269bc5ee1e4d17c8941d
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-22 22:02:56 -0300

    [Java] Section 3 - creating project HelloWorld on intellij

--- udemy_java/section3/class22_Source-code-First-Steps-Variables/HelloWorld/src/Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, Tim");
-
-        int myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.println(myFirstNumber);
-    }
-}
