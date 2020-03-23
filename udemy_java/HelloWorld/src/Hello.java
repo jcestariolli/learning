7def91e00e08fd0d417a04172a91115fabc266f4
Jessica Cestariolli <jessica.cestariolli@gmail.com>
2020-03-22 23:06:12 -0300

    [Java] Section 3 - closing hello world project

--- udemy_java/HelloWorld/src/Hello.java
+++ udemy_java/HelloWorld/src/Hello.java
@@ -1,3 +1,17 @@
 public class Hello {
 
+    public static void main(String[] args) {
+        System.out.println("Hello World da Jeka");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12, myThirdNumber = myFirstNumber * 2;
+        int myTotal = mySecondNumber + myThirdNumber + myFirstNumber;
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+
+    }
+
 }
